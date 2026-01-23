5375585f74a14ca66c34e54b3f358f9c1ce36a03
Suriya C U <suriyac.u.99@gmail.com>
2026-01-23 18:37:37 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_key_pair" "key" {
   key_name   = var.key_name
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = file("C:/Users/suriy/.ssh/id_rsa.pub")
 }
 
 resource "aws_security_group" "sg" {
