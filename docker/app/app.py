32715219b523702940748f7938b0d12df8bb1f72
Suriya C U <suriyac.u.99@gmail.com>
2026-01-23 16:33:00 +0530

    Rename hello function to home and update message

--- docker/app/app.py
+++ docker/app/app.py
@@ -2,8 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello from AWS DevOps Kubernetes Project!"
+def home():
+    return "Hello from AWS DevOps Project!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+app.run(host="0.0.0.0", port=5000)
