53a5ecd8547c282338e58655cc7b978cd868a507
Suriya C U <suriyac.u.99@gmail.com>
2026-01-23 16:28:05 +0530

    Refactor AWS resources and update configurations

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,51 +1,44 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = var.aws_region
 }
 
-resource "aws_key_pair" "devops_key" {
-  key_name   = "devops-key"
-  public_key = file("~/.ssh/id_ed25519.pub")
+resource "aws_key_pair" "key" {
+  key_name   = var.key_name
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
-resource "aws_security_group" "devops_sg" {
+resource "aws_security_group" "sg" {
   name = "devops-sg"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 30000
-    to_port     = 32767
-    protocol    = "tcp"
+    from_port = 30000
+    to_port   = 32767
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_instance" "devops_ec2" {
-  ami             = "ami-03f4878755434977f" # Ubuntu 22.04 LTS (ap-south-1)
-  instance_type   = var.instance_type
-  key_name        = var.key_name
-  security_groups = [aws_security_group.devops_sg.name]
+resource "aws_instance" "ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.key.key_name
+  security_groups = [aws_security_group.sg.name]
 
   tags = {
-    Name = "DevOps-K8s-EC2"
+    Name = "DevOps-EC2"
   }
 }
